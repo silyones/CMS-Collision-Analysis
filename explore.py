3f0d2e8f8e7d592e799ef17be59bb75d4d69aaa4
silyones <trishasilver12@gmail.com>
2026-08-19 23:45:23 +0530

    explore branch

--- explore.py
+++ explore.py
@@ -1,17 +1,9 @@
 import uproot
 
-files = [
-    "opendata/raw/01AB9889-63BA-4171-9842-85AC4E0987DE.root",
-    "opendata/raw/04935F99-9E92-4919-89C2-38670059FBDA.root",
-]
+file = uproot.open("C:/cern/opendata/raw/01AB9889-63BA-4171-9842-85AC4E0987DE.root")
 
-for path in files:
-    print(f"\n=== {path} ===")
-    f = uproot.open(path)
-    print(f.keys())              # top-level objects ('Events')
+print(file.keys())
 
-    events = f["Events"]
-    print(events.num_entries)    # how many collision events in this file
-    print(events.keys()[:30])    # first 30 branch names (things like Muon_pt, Muon_eta, etc.)
-
-
+# print branches
+tree = file["Events"]
+print(tree.keys())
